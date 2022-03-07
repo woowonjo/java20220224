da135577e94d6450c6c82957bf9a60f11aac015a
woowonjo <99157597+woowonjo@users.noreply.github.com>
2022-03-07 09:34:02 +0900

    java

---
+++ src/chap05/book/exercise/Exercise07.java
@@ -0,0 +1,16 @@
+package chap05.book.exercise;
+
+public class Exercise07 {
+	public static void main(String[] args) {
+
+		int[] array = { 1, 5, 3, 8, 2 };
+		int max = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > max) {
+				max = array[i];
+			}
+		}
+		System.out.println("max : " + max);
+	}
+}
