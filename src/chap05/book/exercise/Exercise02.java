c99583718fb9d68c63180b87d7facbd0bdf06044
woowonjo <99157597+woowonjo@users.noreply.github.com>
2022-03-16 08:59:45 +0900

    java

--- src/chap05/book/exercise/Exercise02.java
+++ src/chap05/book/exercise/Exercise02.java
@@ -1,6 +1,6 @@
 package chap05.book.exercise;
 
-public class Ex ercise02 {
+public class Exercise02 {
 	public static void main(String[] args) {
 		int[] arr1 = {3, 2, 1};
 		arr1 = new int[] {99, 88};
