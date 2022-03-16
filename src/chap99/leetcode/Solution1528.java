a507bbd354e41f93bfbf2e71b16bc3faadc069b7
woowonjo <99157597+woowonjo@users.noreply.github.com>
2022-03-16 17:15:39 +0900

    java

---
+++ src/chap99/leetcode/Solution1528.java
@@ -0,0 +1,13 @@
+package chap99.leetcode;
+
+class Solution1528 {
+    public String restoreString(String s, int[] indices) {
+        char[] result = new char[s.length()];
+        
+        for (int i = 0; i < s.length(); i++) {
+            result[indices[i]] = s.charAt(i);
+        }
+        
+        return new String(result);
+    }
+}
---
+++ src/chap99/leetcode/Solution344.java
@@ -0,0 +1,26 @@
+package chap99.leetcode;
+
+public class Solution344 {
+	class Solution {
+		public void reverseString(char[] s) {
+			int left = 0;
+			int right = s.length - 1;
+
+			while (left < right) {
+
+				swap(s, left, right);
+
+				left++;
+				right--;
+
+			}
+
+		}
+
+		private void swap(char[] s, int left, int right) {
+			char temp = s[left];
+			s[left] = s[right];
+			s[right] = temp;
+		}
+	}
+}
